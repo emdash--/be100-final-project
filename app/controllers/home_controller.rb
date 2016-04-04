f6e757d54209b0e43768b3933980ba3d866f585d
Meghan Ventura <meghanventura@gmail.com>
2016-04-04 07:51:02 -0400

    preparing to deploy on Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
   def index
     if params[:search]
       @pokemons = Pokemon.search(params[:search])
-      # render json: @pokemons.map(&:poke_name)
     else
       @pokemons = Pokemon.all
     end
